ef6730b1263e398f04c1030c14134b8a9f357b85
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2025-03-04 13:25:03 +0300

    Handle formwarded messages (#9)

--- features/bot/extract.go
+++ features/bot/extract.go
@@ -1,12 +1,33 @@
 package bot
 
-import "github.com/mvdan/xurls"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/mvdan/xurls"
+)
+
+const (
+	https = "https://"
+	http  = "http://"
+)
+
+func normalizeUrl(url string) string {
+	isOk := strings.HasPrefix(url, https) || strings.HasPrefix(url, http)
+	if isOk {
+		return url
+	}
+	return fmt.Sprintf("%s%s", https, url)
+}
 
 func extract(text string) []string {
-	urls := xurls.Strict.FindAllString(text, -1)
+	urls := xurls.Relaxed.FindAllString(text, -1)
 	if urls == nil {
 		// FindAllString may return nil when no matches
 		urls = []string{}
+	}
+	for i := range urls {
+		urls[i] = normalizeUrl(urls[i])
 	}
 	return urls
 }
