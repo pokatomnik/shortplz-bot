f359fabfd0ffa05a4f33eb742ee5c61c7a3bcd25
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2025-03-04 13:23:44 +0300

    Handle formwarded messages

--- features/bot/cmdurls.go
+++ features/bot/cmdurls.go
@@ -1,16 +1,30 @@
 package bot
 
 import (
+	"strings"
+
 	"gopkg.in/telebot.v4"
 )
 
 func (bot Bot) cmdURLsSetup() {
 	tb := bot.telebot
 
-	tb.Handle(telebot.OnText, func(ctx telebot.Context) error {
-		text := ctx.Message().Text
+	var inTxtHandler = func(ctx telebot.Context) error {
+		var (
+			message       = ctx.Message()
+			caption       = message.Caption
+			text          = message.Text
+			actualCaption = strings.TrimSpace(caption)
+			actualText    = strings.TrimSpace(text)
+			userText      = ""
+		)
+		if actualText != "" {
+			userText = actualText
+		} else if actualCaption != "" {
+			userText = actualCaption
+		}
 
-		urls := extract(text)
+		urls := extract(userText)
 
 		if len(urls) == 0 {
 			return bot.respondZero(ctx)
@@ -22,5 +36,9 @@ func (bot Bot) cmdURLsSetup() {
 		}
 
 		return bot.respondMulti(ctx, urls)
-	})
+	}
+
+	tb.Handle(telebot.OnForward, inTxtHandler)
+
+	tb.Handle(telebot.OnText, inTxtHandler)
 }
